931711bf52c498d80b3efd15d37d1ceaa9748104
Necip Yildiran <fazilyildiran@gmail.com>
2019-06-11 01:40:49 +0300

    new demo: super4pcs with example external point type

---
+++ demos/PointAdapter/ext/pointadapter_extlib1.hpp
@@ -0,0 +1,63 @@
+#ifndef __POINT_ADAPTER_EXTLIB1_H__
+#define __POINT_ADAPTER_EXTLIB1_H__
+
+#include <Eigen/Core>
+#include "point_extlib1.hpp"
+
+namespace extlib1
+{
+
+struct PointAdapter {
+  public:
+    enum {Dim = 3};
+    typedef float Scalar;
+    typedef Eigen::Matrix<Scalar, Dim, 1> VectorType;
+  
+  private:
+    Eigen::Map<const VectorType> m_pos, m_normal, m_color;
+
+    Scalar* underlying_data[3]  = { NULL, NULL, NULL };
+    
+  public:
+    inline PointAdapter(const extlib1::PointType1& p)
+      : m_pos   (Eigen::Map<const VectorType >( p.pos )), 
+        m_normal(Eigen::Map<const VectorType >( p.n )), 
+        m_color (Eigen::Map<const VectorType >( p.color ))
+    { }
+
+
+    inline PointAdapter()
+    : underlying_data{new Scalar[Dim], new Scalar[Dim], new Scalar[Dim]},
+        m_pos   (Eigen::Map<const VectorType >( underlying_data[0] )), 
+        m_normal(Eigen::Map<const VectorType >( underlying_data[1] )), 
+        m_color (Eigen::Map<const VectorType >( underlying_data[2] ))
+    {
+    }
+
+    inline ~PointAdapter()
+    {
+      for(auto &ptr : underlying_data)
+      {
+        if(ptr) delete ptr;
+        ptr = NULL;
+      }
+    }
+
+    inline const Eigen::Map< const VectorType >& pos()    const { return m_pos; }  
+    inline const Eigen::Map< const VectorType >& normal() const { return m_normal; }
+    inline const Eigen::Map< const VectorType >& color()  const { return m_color; }
+    inline const Eigen::Map< const VectorType >& rgb()    const { return m_color; }
+
+/*    inline Scalar& x() { return m_pos.coeffRef(0); }
+    inline Scalar& y() { return m_pos.coeffRef(1); }
+    inline Scalar& z() { return m_pos.coeffRef(2); }
+*/
+
+    inline Scalar x() const { return m_pos.coeff(0); }
+    inline Scalar y() const { return m_pos.coeff(1); }
+    inline Scalar z() const { return m_pos.coeff(2); }
+};
+
+}
+
+#endif
---
+++ demos/PointAdapter/super4pcs_adapter_test.cc
@@ -0,0 +1,258 @@
+#include "ext/point_extlib1.hpp"
+#include "ext/point_extlib2.hpp"
+#include "ext/pointadapter_extlib1.hpp"
+#include "ext/pointadapter_extlib2.hpp"
+
+#include "gr/io/io.h"
+#include "gr/utils/geometry.h"
+#include "gr/sampling.h"
+#include "gr/algorithms/match4pcsBase.h"
+#include "gr/algorithms/Functor4pcs.h"
+#include "gr/algorithms/FunctorSuper4pcs.h"
+#include "gr/algorithms/FunctorBrute4pcs.h"
+#include <gr/algorithms/PointPairFilter.h>
+
+#include <Eigen/Dense>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "../demo-utils.h"
+
+#define sqr(x) ((x) * (x))
+
+using namespace std;
+using namespace gr;
+using namespace gr::Demo;
+
+// create a vector of extlib1::PointType1 from a vector of gr::Point3D 
+std::vector<extlib1::PointType1> getExtlib1Points(std::vector<gr::Point3D> grPoints)
+{
+  std::vector<extlib1::PointType1> result;
+
+  for(auto &p : grPoints)
+  {
+    extlib1::PointType1 newp = { p.pos()(0), p.pos()(1), p.pos()(2),
+                                 p.normal()(0), p.normal()(1), p.normal()(2) };
+
+    result.push_back(newp);
+  }
+
+  return result;
+}
+
+// data IO
+IOManager iomananger;
+
+
+static inline void printS4PCSParameterList(){
+    fprintf(stderr, "\t[ -r result_file_name (%s) ]\n", output.c_str());
+    fprintf(stderr, "\t[ -m output matrix file (%s) ]\n", outputMat.c_str());
+    fprintf(stderr, "\t[ -x (use 4pcs: false by default) ]\n");
+    fprintf(stderr, "\t[ --sampled1 (output sampled cloud 1 -- debug+super4pcs only) ]\n");
+    fprintf(stderr, "\t[ --sampled2 (output sampled cloud 2 -- debug+super4pcs only) ]\n");
+}
+struct TransformVisitor {
+    template <typename Derived>
+    inline void operator()(
+            float fraction,
+            float best_LCP,
+            const Eigen::MatrixBase<Derived>& /*transformation*/) const {
+      if (fraction >= 0)
+        {
+          printf("done: %d%c best: %f                  \r",
+               static_cast<int>(fraction * 100), '%', best_LCP);
+          fflush(stdout);
+        }
+    }
+    constexpr bool needsGlobalTransformation() const { return false; }
+};
+
+template <
+    typename PointType,
+    typename Range,
+    typename Matcher,
+    typename Options,
+    typename Sampler,
+    typename TransformVisitor>
+typename PointType::Scalar computeAlignment (
+    const Options& options,
+    const Utils::Logger& logger,
+    const Range& P,
+    const Range& Q,
+    Eigen::Ref<Eigen::Matrix<typename PointType::Scalar, 4, 4>> mat,
+    const Sampler& sampler,
+    TransformVisitor& visitor
+    ) {
+  Matcher matcher (options, logger);
+  logger.Log<Utils::Verbose>( "Starting registration" );
+  typename PointType::Scalar score = matcher.ComputeTransformation(P, Q, mat, sampler, visitor);
+
+
+  logger.Log<Utils::Verbose>( "Score: ", score );
+  logger.Log<Utils::Verbose>( "(Homogeneous) Transformation from ",
+                              input2.c_str(),
+                              " to ",
+                              input1.c_str(),
+                              ": \n",
+                              mat);
+/*
+  if(! outputSampled1.empty() ){
+      logger.Log<Utils::Verbose>( "Exporting Sampled cloud 1 to ",
+                                  outputSampled1.c_str(),
+                                  " ..." );
+      iomananger.WriteObject((char *)outputSampled1.c_str(),
+                             matcher.getFirstSampled(),
+                             vector<Eigen::Matrix2f>(),
+                             vector<typename Point3D::VectorType>(),
+                             vector<tripple>(),
+                             vector<string>());
+      logger.Log<Utils::Verbose>( "Export DONE" );
+  }
+  if(! outputSampled2.empty() ){
+      logger.Log<Utils::Verbose>( "Exporting Sampled cloud 2 to ",
+                                  outputSampled2.c_str(),
+                                  " ..." );
+      iomananger.WriteObject((char *)outputSampled2.c_str(),
+                             matcher.getSecondSampled(),
+                             vector<Eigen::Matrix2f>(),
+                             vector<typename Point3D::VectorType>(),
+                             vector<tripple>(),
+                             vector<string>());
+      logger.Log<Utils::Verbose>( "Export DONE" );
+  }*/
+
+  return score;
+}
+
+int main(int argc, char **argv) {
+  using namespace gr;
+
+  vector<Point3D> set1, set2;
+  vector<Eigen::Matrix2f> tex_coords1, tex_coords2;
+  vector<typename Point3D::VectorType> normals1, normals2;
+  vector<tripple> tris1, tris2;
+  vector<std::string> mtls1, mtls2;
+
+  // Match and return the score (estimated overlap or the LCP).
+  typename Point3D::Scalar score = 0;
+
+  constexpr Utils::LogLevel loglvl = Utils::Verbose;
+  using SamplerType   = gr::UniformDistSampler;
+  using TrVisitorType = typename std::conditional <loglvl==Utils::NoLog,
+                            DummyTransformVisitor,
+                            TransformVisitor>::type;
+  using PairFilter = gr::AdaptivePointFilter;
+
+  SamplerType sampler;
+  TrVisitorType visitor;
+  Utils::Logger logger(loglvl);
+
+  /// TODO Add proper error codes
+  if(argc < 4){
+      Demo::printUsage(argc, argv);
+      exit(-2);
+  }
+  if(int c = Demo::getArgs(argc, argv) != 0)
+  {
+    Demo::printUsage(argc, argv);
+    printS4PCSParameterList();
+    exit(std::max(c,0));
+  }
+
+  // prepare matcher ressourcesoutputSampled2
+  using MatrixType = Eigen::Matrix<typename Point3D::Scalar, 4, 4>;
+  MatrixType mat (MatrixType::Identity());
+
+  // Read the inputs.
+  if (!iomananger.ReadObject((char *)input1.c_str(), set1, tex_coords1, normals1, tris1,
+                  mtls1)) {
+    logger.Log<Utils::ErrorReport>("Can't read input set1");
+    exit(-1);
+  }
+
+  if (!iomananger.ReadObject((char *)input2.c_str(), set2, tex_coords2, normals2, tris2,
+                  mtls2)) {
+    logger.Log<Utils::ErrorReport>("Can't read input set2");
+    exit(-1);
+  }
+
+  // clean only when we have pset to avoid wrong face to point indexation
+  if (tris1.size() == 0)
+    Utils::CleanInvalidNormals(set1, normals1);
+  if (tris2.size() == 0)
+    Utils::CleanInvalidNormals(set2, normals2);
+
+  //
+  // Convert the points to PointType1 to test adapter
+  //
+  std::vector<extlib1::PointType1> set1_points1 = getExtlib1Points(set1);
+  std::vector<extlib1::PointType1> set2_points1 = getExtlib1Points(set2);
+
+  try {
+
+      if (use_super4pcs) {
+          using MatcherType = gr::Match4pcsBase<gr::FunctorSuper4PCS, extlib1::PointAdapter, TrVisitorType, gr::AdaptivePointFilter, gr::AdaptivePointFilter::Options>;
+          using OptionType  = typename MatcherType::OptionsType;
+
+          OptionType options;
+          if(! Demo::setOptionsFromArgs(options, logger))
+          {
+            exit(-2); /// \FIXME use status codes for error reporting
+          }
+/* template<class PointType, class Range, class Matcher, class Options, class Sampler, class TransformVisitor> typename PointType::Scalar computeAlignment(const Options&, const gr::Utils::Logger&, const Range&, const Range&, Eigen::Ref<Eigen::Matrix<typename PointType::Scalar, 4, 4> >, const Sampler&, TransformVisitor&) */
+          extlib1::PointAdapter::Scalar score = computeAlignment<extlib1::PointAdapter, std::vector<extlib1::PointType1>, MatcherType, OptionType, SamplerType, TrVisitorType> (options, logger, set1_points1, set2_points1, mat, sampler, visitor);
+
+
+      }/*
+      else {
+          using MatcherType = gr::Match4pcsBase<gr::Functor4PCS, gr::Point3D, TrVisitorType, gr::AdaptivePointFilter, gr::AdaptivePointFilter::Options>;
+          using OptionType  = typename MatcherType::OptionsType;
+
+          OptionType options;
+          if(! Demo::setOptionsFromArgs(options, logger))
+          {
+            exit(-2); /// \FIXME use status codes for error reporting
+          }
+
+          score = computeAlignment<MatcherType> (options, logger, set1, set2, mat, sampler, visitor);
+      }*/
+
+  }
+  catch (const std::exception& e) {
+      logger.Log<Utils::ErrorReport>( "[Error]: " , e.what() );
+      logger.Log<Utils::ErrorReport>( "Aborting with code -3 ..." );
+      return -3;
+  }
+  catch (...) {
+      logger.Log<Utils::ErrorReport>( "[Unknown Error]: Aborting with code -4 ..." );
+      return -4;
+  }
+
+/*
+  if(! outputMat.empty() ){
+      logger.Log<Utils::Verbose>( "Exporting Matrix to ",
+                                  outputMat.c_str(),
+                                  "..." );
+      iomananger.WriteMatrix(outputMat, mat.cast<double>(), IOManager::POLYWORKS);
+      logger.Log<Utils::Verbose>( "Export DONE" );
+  }
+
+  if (! output.empty() ){
+
+      logger.Log<Utils::Verbose>( "Exporting Registered geometry to ",
+                                  output.c_str(),
+                                  "..." );
+      Utils::TransformPointCloud(set2, mat);
+      iomananger.WriteObject((char *)output.c_str(),
+                             set2,
+                             tex_coords2,
+                             normals2,
+                             tris2,
+                             mtls2);
+      logger.Log<Utils::Verbose>( "Export DONE" );
+  }
+*/
+  return 0;
+}
