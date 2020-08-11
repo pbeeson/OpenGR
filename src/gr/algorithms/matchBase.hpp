ecc13d3d09cdf2f3a9e9e3820920615576b10d23
Nicolas Mellado <nmellado0@gmail.com>
2020-08-11 17:37:34 +0200

    Check for non-null vector before normalization

--- src/gr/algorithms/matchBase.hpp
+++ src/gr/algorithms/matchBase.hpp
@@ -179,6 +179,7 @@ MATCH_BASE_TYPE::ComputeRigidTransformation(const Coordinates& ref,
     if (vector_p2.squaredNorm() == 0) return std::numeric_limits<Scalar>::max();
     vector_p2.normalize();
     VectorType vector_p3 = vector_p1.cross(vector_p2);
+    if (vector_p3.squaredNorm() == 0) return std::numeric_limits<Scalar>::max();
     vector_p3.normalize();
 
     VectorType vector_q1 = q1 - q0;
@@ -188,6 +189,7 @@ MATCH_BASE_TYPE::ComputeRigidTransformation(const Coordinates& ref,
     if (vector_q2.squaredNorm() == 0) return std::numeric_limits<Scalar>::max();
     vector_q2.normalize();
     VectorType vector_q3 = vector_q1.cross(vector_q2);
+    if (vector_q3.squaredNorm() == 0) return std::numeric_limits<Scalar>::max();
     vector_q3.normalize();
 
     //cv::Mat rotation = cv::Mat::eye(3, 3, CV_64F);
